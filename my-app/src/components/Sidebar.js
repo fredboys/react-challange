43991f1238baf14a577464a747a41b69fe5e2ffa
fredboys <freddieboys@hotmail.co.uk>
2023-02-01 09:54:56 +0000

    Add sidebar function

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div>
+            <a>MyPhotos</a>
+            <a>My Illiustrations</a>
+            <a>My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar
