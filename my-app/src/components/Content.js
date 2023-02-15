9f3b790fdc81d05765d4647307b55867c300f655
fredboys <freddieboys@hotmail.co.uk>
2023-02-15 15:10:03 +0000

    Add loading image to my photos using component mount

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import PostItem from './PostItem';
 import {savedPosts} from "../posts.json";
+import Loader from './Loader';
 
 export class Content extends Component {
 
     constructor(props){
         super(props)
+
+        this.state = {
+          isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          isLoaded: true
+        })
+      }, 2000)
     }
 
   render() {
@@ -33,8 +46,13 @@ export class Content extends Component {
 
         {/* Created using child component */}
         <div className={css.SearchResults}>
+          {
+            this.state.isLoaded ?
             <PostItem savedPosts={savedPosts} />
+            : <Loader />
+          }
         </div>
+
       </div>
     )
   }
